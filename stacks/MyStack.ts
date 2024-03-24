452a6b143bb5901d7f0b2d9b10917036ae214bb6
Jerry Fan <Jerryfan71@gmail.com>
2024-03-24 15:46:29 -0700

    got the sst to bind vite locally

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,4 +1,4 @@
-import { StackContext, Api } from 'sst/constructs';
+import { StackContext, Api, StaticSite } from 'sst/constructs';
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, 'api', {
@@ -7,7 +7,17 @@ export function API({ stack }: StackContext) {
     },
   });
 
+  const web = new StaticSite(stack, 'web', {
+    path: 'packages/web',
+    buildOutput: 'dist',
+    buildCommand: 'npm run build',
+    environment: {
+      VITE_APP_API_URL: api.url,
+    },
+  });
+
   stack.addOutputs({
     ApiEndpoint: api.url,
+    WebsiteURL: web.url,
   });
 }
