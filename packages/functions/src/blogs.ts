024aa5a5f6dd88ee0430fc1ef489a9285ff67d1c
Jerry Fan <Jerryfan71@gmail.com>
2024-04-06 00:12:55 -0700

    Refactor blogs endpoint to handle GET and POST requests

--- packages/functions/src/blogs.ts
+++ packages/functions/src/blogs.ts
@@ -1,10 +1,22 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/aws-lambda';
+import { blogs as blogsTable } from '@my-wishlist-app/core/db/schema/blogs';
+import { db } from '@my-wishlist-app/core/db';
 
 const app = new Hono();
 
-app.get('/blogs', (c) => {
-  return c.json({ message: 'Hello from the frontend!' });
+// get all blogs
+app.get('/blogs', async (c) => {
+  const results = await db.select().from(blogsTable);
+  return c.json({ results });
+});
+
+// post a new blog
+app.post('/blogs', async (c) => {
+  const body = await c.req.json();
+  const blogs = body.blogs;
+  const newBlogs = await db.insert(blogsTable).values(blogs).returning();
+  return c.json({ newBlogs });
 });
 
 export const handler = handle(app);
