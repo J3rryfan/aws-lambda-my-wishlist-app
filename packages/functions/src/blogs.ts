38f6015075ec563e08a2f6571cbf35a75e9c4da9
Jerry Fan <Jerryfan71@gmail.com>
2024-04-07 01:17:03 -0700

    Refactor blogs.ts to use destructuring and add userId to new blog object

--- packages/functions/src/blogs.ts
+++ packages/functions/src/blogs.ts
@@ -2,21 +2,32 @@ import { Hono } from 'hono';
 import { handle } from 'hono/aws-lambda';
 import { blogs as blogsTable } from '@my-wishlist-app/core/db/schema/blogs';
 import { db } from '@my-wishlist-app/core/db';
+import { eq } from 'drizzle-orm';
+
+import { authMiddleware } from '@my-wishlist-app/core/auth';
 
 const app = new Hono();
 
 // get all blogs
-app.get('/blogs', async (c) => {
-  const blogs = await db.select().from(blogsTable);
+app.get('/blogs', authMiddleware, async (c) => {
+  const userId = c.var.userId;
+
+  const blogs = await db
+    .select()
+    .from(blogsTable)
+    .where(eq(blogsTable.userId, userId));
+
   return c.json({ blogs });
 });
 
 // post a new blog
-app.post('/blogs', async (c) => {
+app.post('/blogs', authMiddleware, async (c) => {
+  const userId = c.var.userId;
+
   const body = await c.req.json();
   const blog = {
     ...body.blog,
-    userId: '1',
+    userId: userId,
   };
   const newBlog = await db.insert(blogsTable).values(blog).returning();
   return c.json({ newBlog });
--- packages/web/src/hooks/useCreateBlog.ts
+++ packages/web/src/hooks/useCreateBlog.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
 type Blog = {
   title: string;
@@ -8,16 +9,22 @@ type Blog = {
 
 // creating a blog
 export function useCreateBlog() {
+  const { getToken } = useKindeAuth();
   const navigate = useNavigate({ from: '/create-blog' });
 
   const { mutate, error, isPending } = useMutation({
     mutationFn: async ({ data }: { data: Blog }) => {
+      const token = await getToken();
+      if (!token) {
+        throw new Error('No token');
+      }
       const response = await fetch(
         import.meta.env.VITE_APP_API_URL + '/blogs',
         {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            Authorization: token,
           },
           body: JSON.stringify({ blog: data }),
         }
