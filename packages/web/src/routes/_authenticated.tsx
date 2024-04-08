0c2a9b292aa56cf4594bbb5f97a98e00c711a0de
Jerry Fan <Jerryfan71@gmail.com>
2024-04-07 21:54:04 -0700

    Update login page UI

--- packages/web/src/routes/_authenticated.tsx
+++ packages/web/src/routes/_authenticated.tsx
@@ -2,14 +2,17 @@ import { createFileRoute, Outlet } from '@tanstack/react-router';
 import { Button } from '@/components/ui/button';
 
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
+// import Logo from '@/components/logo';
 
 export function Login() {
   const { login, register } = useKindeAuth();
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className='text-4xl font-bold'>Welcome to My Wishlist</h1>
-      <p className='text-xl'>Please login to continue</p>
-      <div className='mt-8 flex flex-col gap-y-4'>
+    <div className='flex flex-col h-dvh items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Welcome to WishList </h1>
+      <p className='text-xl mt-5 text-muted-foreground'>
+        A app where you can post your wishlist
+      </p>
+      <div className='mt-8 flex items-center justify-center gap-x-4'>
         <Button onClick={() => login()}>Login</Button>
         <Button onClick={() => register()}>Register</Button>
       </div>
