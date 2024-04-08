6e47c9fbcfd1008f8cab1b0d5cec5989b56c2d19
Jerry Fan <Jerryfan71@gmail.com>
2024-04-08 12:04:20 -0700

    Refactor profile page UI and add user avatar and card component

--- packages/web/src/routes/_authenticated/profile.tsx
+++ packages/web/src/routes/_authenticated/profile.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/components/ui/button';
 
 import { createFileRoute } from '@tanstack/react-router';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export const Route = createFileRoute('/_authenticated/profile')({
   component: Profile,
@@ -9,11 +11,33 @@ export const Route = createFileRoute('/_authenticated/profile')({
 
 function Profile() {
   const { logout, user } = useKindeAuth();
+
+  if (!user || !user.picture) {
+    throw new Error('User not found');
+  }
+
   return (
     <div className='flex flex-col gap-y-4 items-center'>
-      <h1 className='text-4xl font-bold'>Hi {user?.given_name}</h1>
-      <div className='text-2xl font-bold'>{user?.email}</div>
-      <Button onClick={() => logout()}>Logout</Button>
+      <Card className='w-full max-w-lg mx-auto'>
+        <CardHeader className='flex flex-col items-center p-6'>
+          <Avatar className='m-0'>
+            <AvatarImage src={user.picture} alt='profile logo' />
+            <AvatarFallback>{user.given_name}</AvatarFallback>
+          </Avatar>
+          <div className='flex flex-col gap-1 text-center'>
+            <h2 className='text-lg font-bold'>{user.given_name}</h2>
+            <p className='text-sm text-gray-500 dark:text-gray-400'>
+              @{user.given_name}
+            </p>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>Rockstar. Loves snakes. Welcome to my nightmare</p>
+        </CardContent>
+        <div className='flex items-center justify-end gap-x-4'>
+          <Button onClick={() => logout()}>Logout</Button>
+        </div>
+      </Card>
     </div>
   );
 }
